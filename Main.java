3685c5ca10fcf431e41fe185c75ed0a0908f7e96
Krzysztof Nazar <s184698@student.pg.edu.pl>
2022-07-11 12:27:34 +0200

    second commit

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hellooooooo");
+        System.out.println("hellooooooo first");
+
+        System.out.println("hellooooooo second");
+
     }
 }
